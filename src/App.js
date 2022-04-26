88449868adad2fd87699260d42b8c3aebd70d9ca
ManuelAlejandroCG <manuelalejandro.cg@gmail.com>
2022-04-26 18:40:51 -0300

    Agregada prop Greeting

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer/>
+      <ItemListContainer greeting='Mi catalogo de productos'/>
     </div>
   );
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
-const ItemListContainer = () => {
-    return (
-        <div>
-            <h1>Lista de productos</h1>
-            <p>producto 1</p>
-            <p>producto 2</p>
-            <p>producto 3</p>
-            <p>producto 4</p>
-            <p>producto 5</p>
-        </div>
-    )
+// const ItemListContainer = () => {
+//     return (
+//         <div>
+//             <h1>Lista de productos</h1>
+//             <p>producto 1</p>
+//             <p>producto 2</p>
+//             <p>producto 3</p>
+//             <p>producto 4</p>
+//             <p>producto 5</p>
+//         </div>
+//     )
+// }
+
+function ItemListContainer ({greeting}) {
+    return (greeting)
 }
 
 export default ItemListContainer;
