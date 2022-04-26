e9c39c62c6ecbd6b125d08f78f68c0915082e35b
ManuelAlejandroCG <manuelalejandro.cg@gmail.com>
2022-04-26 20:36:39 -0300

    proyecto 3.1 prueba de ItemCount

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer greeting='Mi catalogo de productos'/>
+      <ItemCount/>
     </div>
   );
 }
--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import './ItemCount.css'
 
-export default function ItemCount() {
+function ItemCount() {
+  const [count, setCount] = useState(0);
+  const [stock, setStock] = useState(10);
   return (
-    <div>
-        <button>-</button>
-        <button>+</button>
-    </div>
+    <Card id="contenedor" lassName="text-center">
+
+      <Card.Header>Intel i7 7700k</Card.Header>
+      <Card.Body>
+        <Card.Text>7th Generation Intel® Core™ i7 Processors</Card.Text>
+        <Card.Title>Stock {stock}</Card.Title>
+        <div id="botones">
+          <Button variant="primary" onClick={() => { count > 0 ? (setCount(count - 1), setStock(stock + 1)) : (alert("el carrito ya esta vacio")) }}>-</Button>
+          <Card.Text>
+            cantidad a comprar {count}
+          </Card.Text>
+          <Button variant="primary" onClick={() => { stock > 0 ? (setCount(count + 1), setStock(stock - 1)) : (alert("no hay mas stock")) }}>+</Button>
+        </div>
+      </Card.Body>
+    </Card>
+
   )
 }
+
+export default ItemCount;
